cc38a4270ab8468a267d7a1a41acd3d24f278b58
Kamboji Pranav <kambojipranav@gmail.com>
2025-07-03 18:05:47 +0530

    Update 1_🏠_Dashboard.py

--- pages/1_🏠_Dashboard.py
+++ pages/1_🏠_Dashboard.py
@@ -2,14 +2,16 @@ import streamlit as st
 from data_loader import load_data
 import plotly.express as px
 
-st.set_page_config(page_title="📈 Dashboard")
-
-st.title("📊 Uber Daily Trips Overview")
+st.set_page_config(page_title="📊 Dashboard", layout="wide")
+st.title("📊 Uber Trip Dashboard")
 
 df = load_data()
 
-st.subheader("Daily Trip Count")
-fig = px.line(df, x='Date', y='Trips', title='Uber Daily Trips - Jan & Feb')
-st.plotly_chart(fig, use_container_width=True)
+if not df.empty:
+    st.subheader("Trips over Time")
+    fig = px.line(df, x='date', y='trips', title='Trips Over Time')
+    st.plotly_chart(fig, use_container_width=True)
 
-st.dataframe(df.tail(10), use_container_width=True)
+    st.subheader("Active Vehicles vs Trips")
+    fig2 = px.scatter(df, x='active_vehicles', y='trips', color='dispatching_base_number', title='Active Vehicles vs Trips')
+    st.plotly_chart(fig2, use_container_width=True)
