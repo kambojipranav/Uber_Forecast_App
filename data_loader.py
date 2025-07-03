b2e9dd727c18877f9985522adf7de6cc87968065
Kamboji Pranav <kambojipranav@gmail.com>
2025-07-03 18:04:43 +0530

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -4,9 +4,13 @@ import pandas as pd
 @st.cache_data
 def load_data():
     df = pd.read_csv("Uber-Jan-Feb-FOIL.csv")
-    df['Date/Time'] = pd.to_datetime(df['Date/Time'], errors='coerce')
-    df = df.dropna(subset=['Date/Time'])
-    df['Date'] = df['Date/Time'].dt.date
-    daily_data = df.groupby('Date').size().reset_index(name='Trips')
-    daily_data['Date'] = pd.to_datetime(daily_data['Date'])
-    return daily_data
+    df.rename(columns=lambda x: x.strip().lower(), inplace=True)
+
+    if 'date' in df.columns:
+        df['date'] = pd.to_datetime(df['date'], errors='coerce')
+        df.dropna(subset=['date'], inplace=True)
+    else:
+        st.error("Missing 'date' column in uploaded data.")
+        return pd.DataFrame()
+
+    return df
